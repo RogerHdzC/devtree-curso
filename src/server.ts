37ae43f7c1107473620819a346e454462fd12900
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-13 20:54:44 -0600

    feat(server): initilize db connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import 'dotenv/config'
 import router from './router'
+import { connectDB } from './config/db'
 const app = express()
+connectDB()
 // To read data client-server
 app.use(express.json())
 app.use('/',router)
