67f084300269981f58a529cd75f354d1fe741412
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-14 23:08:15 -0600

    feat(handlers): add logic for duplicate registers

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,16 @@
-import User from "../models/User"
 import { Request, Response } from "express"
+import User from "../models/User"
+
 export const createAccount = async (request: Request, response: Response) => {
+    const {email} = request.body
+    const userExists = await User.findOne({email})
+    if(userExists){
+        const error = new Error('El Usuario ya esta registrado')
+        response.status(409).json({error: error.message})
+        return;
+    }
     const user = new User(request.body)
     await user.save()
 
-    response.send('Registro Creado Correctamente')
+    response.status(201).send('Registro Creado Correctamente')
 }
