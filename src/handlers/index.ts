923aa4f15583130388a392ffb99fb776b1603be6
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-14 23:28:54 -0600

    feat(hanlders): add logic to check unique handlers and format handlers

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import slug from 'slug'
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 
@@ -10,8 +11,16 @@ export const createAccount = async (request: Request, response: Response) => {
         response.status(409).json({error: error.message})
         return;
     }
+    const handle = slug(request.body.handle, '')
+    const handleExists = await User.findOne({handle})
+    if(handleExists){
+        const error = new Error('Nombre de usuario no disponible')
+        response.status(409).json({error: error.message})
+        return;
+    }
     const user = new User(request.body)
     user.password = await hashPassword(password)
+    user.handle = handle
     await user.save()
 
     response.status(201).send('Registro Creado Correctamente')
