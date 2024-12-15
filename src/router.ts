a13aab0ac0f67c4842e4c25f07c4a69bc5f9b2af
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-14 23:43:45 -0600

    feat(error-validator): add validar for register on server side

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 /** Auth and Register */
-router.post('/auth/register', createAccount)
+router.post('/auth/register', 
+    body('handle').
+        notEmpty().
+        withMessage('El handle no puede ir vacío'),
+    body('name').
+        notEmpty().
+        withMessage('El name no puede ir vacío'),
+    body('password').
+        isLength({min: 8}).
+        withMessage('El password es muy corto, mínumo 8 caracteres'),
+    body('email').
+        isEmail().
+        withMessage('El mail no es válido'),
+    createAccount)
 
 export default router
