d8004629a072855123a19eb9b6f961fb55fbfbd5
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-14 23:16:35 -0600

    feat(hash): add utility to hash password

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (request: Request, response: Response) => {
-    const {email} = request.body
+    const {email, password} = request.body
     const userExists = await User.findOne({email})
     if(userExists){
         const error = new Error('El Usuario ya esta registrado')
@@ -10,6 +11,7 @@ export const createAccount = async (request: Request, response: Response) => {
         return;
     }
     const user = new User(request.body)
+    user.password = await hashPassword(password)
     await user.save()
 
     response.status(201).send('Registro Creado Correctamente')
