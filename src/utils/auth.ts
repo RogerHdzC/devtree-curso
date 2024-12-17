e28e6fc5fcb3107d9121a487189f7ef87fc2dcad
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-16 23:00:53 -0600

    feat(auth): add check password function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,3 +4,7 @@ export const hashPassword = async (password : string) => {
     const salt = await bcrypt.genSalt(10)
     return await bcrypt.hash(password, salt)
 }
+
+export const checkPassword = async (entryPassword : string, hash : string) => {
+    return await bcrypt.compare(entryPassword, hash)
+}
