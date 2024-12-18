9c9e60bbeba5122a87c8d150f6b996fedc150d75
Rogelio Hdz C <rhclcc@yahoo.com.mx>
2024-12-17 23:25:24 -0600

    feat(cors): add configand package related to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 import router from './router'
 import { connectDB } from './config/db'
-const app = express()
+import { corsConfig } from './config/cors'
 connectDB()
+const app = express()
+app.use(cors(corsConfig))
 // To read data client-server
 app.use(express.json())
 app.use('/',router)
